b04945e6a1bbf3408fe4963ef31a5bf3280e7c59
Akshay Anand <86616444+Akshay2931@users.noreply.github.com>
2023-08-08 17:04:55 +0530

    input output commit

---
+++ learning/hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(){
+
+    cout << "Hello, World!! " << endl;
+    printf("Hello, World!! from C");
+    return 0;
+    
+}
---
+++ learning/io.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+
+    // Single line comments
+    /* Multiline 
+    comments*/
+
+    int favourite_num {0};
+    std::cout << "Enter your favourite number between 1 and 100 " << std::endl ;
+    std::cin >> favourite_num;
+    std::cout << "Your favourite number is: " << favourite_num << std::endl;
+    return 0;
+
+}
